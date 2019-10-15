af529fb2ded43de224a6e5e910b2b65e2d86579a
bosmanpa <bsmn.ptrck@gmail.com>
2019-10-15 14:59:47 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name, age, gender, color, temperament, alive) values ('Mr. Chocolate', 25, 'Male', 'Brown', 'angry', 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Rowdy', 20, 'Female', 'Black', 'rowdy', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Tabitha', 15, 'Female', 'Brown', 'calm', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Sergeant Brown', 12, 'Male', 'Brown', 'disciplined', 0);
+insert into bears (name, age, gender, color, temperament, alive) values ('Melissa', 20, 'Female', "Black", 'nuturing', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Grinch', 30, 'Male', 'Black', 'grinchy', 1);
+insert into bears (name, age, gender, color, temperament, alive) values ('Wendy', 22, 'Female', "Brown", 'spicy', 1);
+insert into bears (name, age, gender, color, temperament, alive) values (NULL, 26, 'Male', 'Brown', 'hungry', 0);
