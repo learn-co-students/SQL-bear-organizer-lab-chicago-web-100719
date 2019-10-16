c4c0768263707eaddd80c84680198aed517f8958
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-10-16 11:12:01 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (1, 'Johnny', 7, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (2, 'Kika', 2, 'F', 'Brown', 'Lazy', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (3, 'Alexandra', 9, 'F', 'Black', 'Calm', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (4, 'Peter', 4, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (5, 'Alex', 6, 'M', 'Black', 'Aggresive', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (6, 'Tata', 10, 'M', 'Black', 'Aggresive', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (7, 'Mama', 10, 'F', 'White', 'Calm', 1);
+INSERT INTO bears (id, name, age, gender, color,temperament, alive) VALUES (8, NULL, 7, 'M', 'Brown', 'Lazy', 1);
