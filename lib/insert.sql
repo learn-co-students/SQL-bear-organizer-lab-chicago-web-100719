b6178de1b8b4657d73c1af9c43d1fefdbe687e0b
Nathan Alex <nate745@gmail.com>
2019-10-16 13:11:42 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocoloate',5,'Male','Black','Cool','False');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Rowdy',6,'Male','Brown','Angry','True');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha',6,'Female','Brown','Sweet','True');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown',6,'Male','Black','Kind','False');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa',13,'Female','Pink','Sleepy','True');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Grinch',8,'Male','Black','Shy','True');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy',6,'Female','Brown','Slow','True');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,5,'Male','Black','Cool','False');
+
