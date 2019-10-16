f36094f26c1c2a79088648d24901d28dc2a4c15d
NaveedG <nganjani@protonmail.com>
2019-10-16 13:42:32 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES('bear', 5, 'male', 'brown', 'relaxed', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES(null, 5, 'male', 'brown', 'relaxed', true);
